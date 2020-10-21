fd35ca40eb4d516eb3ea51115e1745027c3e683a
Eugene <evolokhin@gmail.com>
2020-10-21 18:11:28 +0300

    Updates

---
+++ precache-manifest.d98c2395fec446c21730799aab5cdc86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15064fd6a3a353efa050b5e4121acd55",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e7016202b4abe93b29",
+    "url": "./static/js/main.93912456.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.d98c2395fec446c21730799aab5cdc86.js"
 );
 
 self.addEventListener('message', (event) => {
